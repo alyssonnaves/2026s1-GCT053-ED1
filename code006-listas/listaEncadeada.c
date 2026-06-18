6b7877a306cea2428919cf40580ab9e4e83f6b92
Alysson Naves <alysson.naves@ectare.com.br>
2026-06-18 08:14:26 -0300

    aula 17/06/2026

--- code006-listas/listaEncadeada.c
+++ code006-listas/listaEncadeada.c
@@ -54,11 +54,54 @@ No* inserir_inicio(No *head, int valor){
     return head;
 }
 
+No* inserir_fim(No *head, int valor){
+    No *novo = criar_no(valor);
+    if (novo == NULL){
+        return head;
+    }
+    if (head == NULL){ //  se a lista vazia!!
+        imprimir_lista(novo);
+        return novo;
+    }
+    // percorrer ate o fim da lista
+    No *atual = head;
+    while (atual->prox != NULL){
+        atual = atual->prox;
+    }
+    atual->prox = novo;
+    imprimir_lista(head);
+    return head;
+}
+
+No* inserir_posicao(No *head, int valor, int posicao){
+    if (posicao <= 0){
+        return inserir_inicio(head, valor);
+    }
+    No *atual = head;
+    int contador = 0;
+    while (atual != NULL && contador < posicao-1){
+        atual = atual->prox;
+        contador++;
+    }
+    if (atual == NULL){
+        return inserir_fim(head, valor);
+    }
+    // achamos a posicao de insercao
+    No *novo = criar_no(valor);
+    novo->prox = atual->prox;
+    atual->prox = novo;
+    imprimir_lista(head);
+    return head;
+}
+
 int main(){
     No *lista = NULL;
     // inserir no inicio
     lista = inserir_inicio(lista, 30);
     lista = inserir_inicio(lista, 20);
     lista = inserir_inicio(lista, 10);
+    lista = inserir_fim(lista, 100);
+    lista = inserir_fim(lista, 200);
+    lista = inserir_posicao(lista, 50, 3);
     return 0;
 }
